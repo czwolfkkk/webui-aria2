f3b715aa2e5daf0c4465346c0056b96e3b7521e6
czwolfkkk <45241019+czwolfkkk@users.noreply.github.com>
2019-02-15 12:54:14 +0800

    Update configuration.js

--- src/js/services/configuration.js
+++ src/js/services/configuration.js
@@ -9,11 +9,11 @@ export default angular
     // default authentication configuration, never fill it in case the webui is hosted in public IP as it can be compromised
     host: location.protocol.startsWith("http") ? location.hostname : "localhost",
     path: "/jsonrpc",
-    port: 6800,
+    port: 65000,
     encrypt: false,
     auth: {
       // either add the token field or the user and pass field, not both.
-      // token: '$YOUR_SECRET_TOKEN$'
+         token: '$YOUR_SECRET_TOKEN$'
       /*-----------------------------*/
       // user: '*YOUR_USERNAME*',
       // pass: '*YOUR_SECRET_PASS*'
